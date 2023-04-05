29d2190f7c1fced138504a4f0e0057562743a479
서나무 <104984874+ixio0330@users.noreply.github.com>
2023-04-05 22:06:37 +0900

    👽 gen style

---
+++ src/styles/global/borderRadius.js
@@ -0,0 +1,28 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 05 Apr 2023 12:58:26 GMT
+ */
+
+module.exports = {
+  "borderRadius": {
+    "default": {
+      "value": 20,
+      "type": "borderRadius",
+      "filePath": "tokens/global.json",
+      "isSource": true,
+      "original": {
+        "value": 20,
+        "type": "borderRadius"
+      },
+      "name": "BorderRadiusDefault",
+      "attributes": {
+        "category": "borderRadius",
+        "type": "default"
+      },
+      "path": [
+        "borderRadius",
+        "default"
+      ]
+    }
+  }
+};
---
+++ src/styles/global/sizing.js
@@ -0,0 +1,202 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 05 Apr 2023 12:58:26 GMT
+ */
+
+module.exports = {
+  "sizing": {
+    "font": {
+      "scale": {
+        "value": 1.125,
+        "type": "sizing",
+        "filePath": "tokens/global.json",
+        "isSource": true,
+        "original": {
+          "value": 1.125,
+          "type": "sizing"
+        },
+        "name": "SizingFontScale",
+        "attributes": {
+          "category": "sizing",
+          "type": "font",
+          "item": "scale"
+        },
+        "path": [
+          "sizing",
+          "font",
+          "scale"
+        ]
+      }
+    },
+    "size": {
+      "8": {
+        "value": 8,
+        "type": "sizing",
+        "filePath": "tokens/global.json",
+        "isSource": true,
+        "original": {
+          "value": 8,
+          "type": "sizing"
+        },
+        "name": "SizingSize8",
+        "attributes": {
+          "category": "sizing",
+          "type": "size",
+          "item": "8"
+        },
+        "path": [
+          "sizing",
+          "size",
+          "8"
+        ]
+      },
+      "16": {
+        "value": 16,
+        "type": "sizing",
+        "filePath": "tokens/global.json",
+        "isSource": true,
+        "original": {
+          "value": 16,
+          "type": "sizing"
+        },
+        "name": "SizingSize16",
+        "attributes": {
+          "category": "sizing",
+          "type": "size",
+          "item": "16"
+        },
+        "path": [
+          "sizing",
+          "size",
+          "16"
+        ]
+      },
+      "24": {
+        "value": 24,
+        "type": "sizing",
+        "filePath": "tokens/global.json",
+        "isSource": true,
+        "original": {
+          "value": 24,
+          "type": "sizing"
+        },
+        "name": "SizingSize24",
+        "attributes": {
+          "category": "sizing",
+          "type": "size",
+          "item": "24"
+        },
+        "path": [
+          "sizing",
+          "size",
+          "24"
+        ]
+      },
+      "32": {
+        "value": 32,
+        "type": "sizing",
+        "filePath": "tokens/global.json",
+        "isSource": true,
+        "original": {
+          "value": 32,
+          "type": "sizing"
+        },
+        "name": "SizingSize32",
+        "attributes": {
+          "category": "sizing",
+          "type": "size",
+          "item": "32"
+        },
+        "path": [
+          "sizing",
+          "size",
+          "32"
+        ]
+      },
+      "40": {
+        "value": 40,
+        "type": "sizing",
+        "filePath": "tokens/global.json",
+        "isSource": true,
+        "original": {
+          "value": 40,
+          "type": "sizing"
+        },
+        "name": "SizingSize40",
+        "attributes": {
+          "category": "sizing",
+          "type": "size",
+          "item": "40"
+        },
+        "path": [
+          "sizing",
+          "size",
+          "40"
+        ]
+      },
+      "48": {
+        "value": 48,
+        "type": "sizing",
+        "filePath": "tokens/global.json",
+        "isSource": true,
+        "original": {
+          "value": 48,
+          "type": "sizing"
+        },
+        "name": "SizingSize48",
+        "attributes": {
+          "category": "sizing",
+          "type": "size",
+          "item": "48"
+        },
+        "path": [
+          "sizing",
+          "size",
+          "48"
+        ]
+      },
+      "56": {
+        "value": 56,
+        "type": "sizing",
+        "filePath": "tokens/global.json",
+        "isSource": true,
+        "original": {
+          "value": 56,
+          "type": "sizing"
+        },
+        "name": "SizingSize56",
+        "attributes": {
+          "category": "sizing",
+          "type": "size",
+          "item": "56"
+        },
+        "path": [
+          "sizing",
+          "size",
+          "56"
+        ]
+      },
+      "64": {
+        "value": 64,
+        "type": "sizing",
+        "filePath": "tokens/global.json",
+        "isSource": true,
+        "original": {
+          "value": 64,
+          "type": "sizing"
+        },
+        "name": "SizingSize64",
+        "attributes": {
+          "category": "sizing",
+          "type": "size",
+          "item": "64"
+        },
+        "path": [
+          "sizing",
+          "size",
+          "64"
+        ]
+      }
+    }
+  }
+};
